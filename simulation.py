d8c4f45a34e5ee48e983fd365f9ff6f061a95345
MaherDissem <maherdissem26@gmail.com>
2024-05-14 11:52:54 -0400

    added custom strategy

--- simulation.py
+++ simulation.py
@@ -1,14 +1,22 @@
 import flwr as fl
 from client import client_fn
 from config import NUM_CLIENTS, client_resources
-from server import strategy
+from server import fedavg_strategy
+from server import FedCustom
 
 
-# Start simulation
+# fl.simulation.start_simulation(
+#     client_fn=client_fn,
+#     num_clients=NUM_CLIENTS,
+#     config=fl.server.ServerConfig(num_rounds=3),
+#     strategy=fedavg_strategy,
+#     client_resources=client_resources,
+# )
+
 fl.simulation.start_simulation(
     client_fn=client_fn,
     num_clients=NUM_CLIENTS,
     config=fl.server.ServerConfig(num_rounds=3),
-    strategy=strategy,
+    strategy=FedCustom(),  # <-- pass the new strategy here
     client_resources=client_resources,
 )

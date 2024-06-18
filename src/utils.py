a726f6c5b2d38520a87cdb25fe807499c7beecab
MaherDissem <maherdissem26@gmail.com>
2024-06-18 13:12:18 -0400

    disable cudnn benchmark

--- src/utils.py
+++ src/utils.py
@@ -25,7 +25,7 @@ def set_seed(seed: int):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
     torch.cuda.manual_seed(seed) 
     torch.cuda.manual_seed_all(seed)
     
